83f039f600dc627316a2a998a956cdb6acca2043
DevOfHockey <liam.conkin@gmail.com>
2023-02-03 01:21:08 -0800

    first commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Eniqueue Styles */
+
+if(!function_exists('oneplant_styles')) :
+
+    function oneplant_styles() {
+        wp_enqueue_style( 'oneplant-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'));
+    }
+    
+endif;
+
+add_action('wp_enqueue_scripts', 'oneplant_styles');
